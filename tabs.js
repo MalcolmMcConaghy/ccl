fa1b922209559f9a220da729eb3d31229d60ee12
Malcolm <mcconaghym@live.co.uk>
2019-06-20 23:54:53 +0100

    Added js functionality to change active tab

--- tabs.js
+++ tabs.js
@@ -1,1 +1,24 @@
-console.log('log from tabs.js');
+document.addEventListener("DOMContentLoaded", function() { 
+  var tabHeaders = document.getElementsByClassName('tabs__header');
+  var tabContent = document.getElementsByClassName('tabs__content');
+
+  function openTab(tabId) {
+    for (i = 0; i < tabHeaders.length; i++) {
+      tabHeaders[i].classList.remove('tabs__header--active');
+    }
+
+    for (i = 0; i < tabContent.length; i++) {
+      tabContent[i].classList.remove('tabs__content--active');
+    }
+
+    event.target.classList.add('tabs__header--active');
+
+    document.getElementById('tabs__content--' + tabId).classList.add('tabs__content--active');
+  }
+
+  for (i = 0; i < tabHeaders.length; i++) {
+    tabHeaders[i].addEventListener('click', function() {
+      openTab(this.innerHTML.replace(/\s+/g, '-').toLowerCase());
+    });
+  }
+});
